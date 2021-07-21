14ecfba0f0c7d8a7b0ff77d1a654b1cd21c30576
Mert Büyük <63505261+mertbuyuk@users.noreply.github.com>
2021-07-21 13:38:50 +0300

    create data class

---
+++ app/src/main/java/com/mb/todolistmvvm/data/Task.kt
@@ -0,0 +1,20 @@
+package com.mb.todolistmvvm.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "task")
+@Parcelize
+data class Task(
+    val task : String,
+    val important : Boolean,
+    val check : Boolean,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id : Int = 0
+) : Parcelable{
+    val createdDateFormat : String
+        get() = DateFormat.getDateInstance().format(created)
+}
