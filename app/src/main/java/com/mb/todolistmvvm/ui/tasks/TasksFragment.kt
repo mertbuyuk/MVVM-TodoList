46386fcecac420d827b84472de5582aa8a1ce6e5
Mert Büyük <63505261+mertbuyuk@users.noreply.github.com>
2021-07-28 13:49:09 +0300

    Saving data with DataStore

--- app/src/main/java/com/mb/todolistmvvm/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/mb/todolistmvvm/ui/tasks/TasksFragment.kt
@@ -1,19 +1,22 @@
 package com.mb.todolistmvvm.ui.tasks
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mb.todolistmvvm.R
+import com.mb.todolistmvvm.data.SortOrder
 import com.mb.todolistmvvm.databinding.FragmentTasksBinding
 import com.mb.todolistmvvm.util.queryChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
@@ -48,22 +51,27 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         searchView.queryChanged {
             viewModel.search.value = it
         }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.actionHide).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.actionSortByDate->{
-                viewModel.sortOrder.value = SortOrder.BY_DATE
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                 true
             }
             R.id.actionSortByName->{
-                viewModel.sortOrder.value = SortOrder.BY_NAME
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
             R.id.actionHide->{
                 item.isChecked = !item.isChecked
-                Log.i("ischeck",item.isChecked.toString())
-                viewModel.hide.value = item.isChecked
+
+                viewModel.onHideCompletedSelected(item.isChecked)
                 true
             }
             R.id.actionDeleteCompleted->{
--- app/src/main/java/com/mb/todolistmvvm/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mb/todolistmvvm/ui/tasks/TasksViewModel.kt
@@ -1,34 +1,44 @@
 package com.mb.todolistmvvm.ui.tasks
 
 import androidx.lifecycle.ViewModel
-import com.mb.todolistmvvm.data.TasksDao
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.mb.todolistmvvm.data.PreferencesManager
+import com.mb.todolistmvvm.data.SortOrder
+import com.mb.todolistmvvm.data.TasksDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
-    tasksDao: TasksDao
+    tasksDao: TasksDao,
+    private val preferencesManager: PreferencesManager
 ) :ViewModel(){
-    val search = MutableStateFlow("")
 
-    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hide = MutableStateFlow(false)
+    val search = MutableStateFlow("")
+    val preferencesFlow = preferencesManager.preferencesFlow
 
     private val taskFlow = combine(
         search,
-        sortOrder,
-        hide
-    ){search,sortOrder,hide ->
-        Triple(search,sortOrder,hide)
-    }.flatMapLatest {(search,sortOrder,hide)->
-        tasksDao.getAllTask(search,sortOrder,hide)
+        preferencesFlow
+    ){search,filteredPrefs->
+        Pair(search,filteredPrefs)
+    }.flatMapLatest {(search,filteredPrefs)->
+        tasksDao.getAllTask(search,filteredPrefs.sortOrder,filteredPrefs.hideCompleted)
     }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedSelected(hideCompleted : Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
+
     val tasks = taskFlow.asLiveData()
 }
 
-enum class SortOrder{BY_DATE, BY_NAME}
